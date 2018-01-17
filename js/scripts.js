46f5905b09fe71b89310bfa6f9c087f245031626
Chris Swanner and Joel Adams <joel@>
2018-01-17 14:20:58 -0800

    add hella stylin

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,40 @@
 $(document).ready(function() {
   $("form#stressSurvey").submit(function(event) {
-  var add = function(array) {
-    return n1 + n2 + n3 + n4 + n5 + n6 + n7 + n8 + n9 + n10 + n11 + n12 + n13 + n14 + n15;
-  }
-  var resultArray = [];
-  var warningArray = [];
-  var symptomArray = [];
-  var copingArray = [];
 
-  $("#stress-responses").show();
+  var resultArray = [];
+
+  $("#stressSurvey").hide();
   $("input:checkbox[name=warningSigns]:checked").each(function() {
     var warningResult = parseInt($(this).val());
     resultArray.push(warningResult);
   });
+
   $("input:checkbox[name=symptoms]:checked").each(function() {
     var symptomResult = parseInt($(this).val());
     resultArray.push(symptomResult);
-
-
   });
+
   $("input:checkbox[name=copingMethods]:checked").each(function() {
     var copingResult = parseInt($(this).val());
     resultArray.push(copingResult);
-
-
   });
+
   var result = resultArray.reduce(function (a, b) {
     return a + b;
   });
-  alert(result);
+
+  if (result === -5) {
+    $("#stressBuddha").show();
+  } else if (result <= 0){
+    $("#stress0").show();
+  } else if (result <= 5){
+    $("#stress5").show();
+  } else if (result <= 9){
+    $("#stress9").show();
+  } else if (result === 10){
+    $("#stressMidnight").show();
+  }
+
   event.preventDefault();
   });
 });
